d251582ecc946208c7b95d08a4c1dacf30d019de
Your Name~ <wgcampos15@gmail.com>
2022-04-15 22:33:57 -0500

    tablemvt?

--- src/main/java/controll/PrimaryController.java
+++ src/main/java/controll/PrimaryController.java
@@ -1,19 +1,28 @@
 package controll;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import model.TableMVT;
 
-public class PrimaryController {
+public class PrimaryController implements Initializable {
 
     @FXML
     private Button btnstep;
 
     @FXML
-    private TableColumn<?, ?> duracion;
+    private TableColumn<TableMVT, Integer> duracion;
 
     @FXML
     private HBox hboxMain;
@@ -25,13 +34,16 @@ public class PrimaryController {
     private HBox hboxright;
 
     @FXML
-    private TableColumn<?, ?> llegada;
+    private TableColumn<TableMVT, Integer> llegada;
 
     @FXML
-    private TableColumn<?, ?> proceso;
+    private TableColumn<TableMVT, String> proceso;
 
     @FXML
-    private TableColumn<?, ?> tamanio;
+    private TableView<TableMVT> tablaMuestra;
+
+    @FXML
+    private TableColumn<TableMVT, Integer> tamanio;
 
     @FXML
     private Label title;
@@ -47,5 +59,25 @@ public class PrimaryController {
 
     @FXML
     private VBox vboxFatherTables;
+
+    ObservableList<TableMVT> list = FXCollections.observableArrayList(
+
+        new TableMVT("A", 8000, 1, 7),
+        new TableMVT("B", 14000, 2, 7),
+        new TableMVT("C", 18000, 3, 4),
+        new TableMVT("D", 6000, 4, 6),
+        new TableMVT("E", 14000, 5, 5)
+    );
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        // TODO Auto-generated method stub
+
+        proceso.setCellValueFactory(new PropertyValueFactory<TableMVT,String>("proceso"));
+        tamanio.setCellValueFactory(new PropertyValueFactory<TableMVT,Integer>("tamanio"));
+        llegada.setCellValueFactory(new PropertyValueFactory<TableMVT,Integer>("llegada"));
+        duracion.setCellValueFactory(new PropertyValueFactory<TableMVT,Integer>("duracion"));
+        tablaMuestra.setItems(list);
+    }
 
 }
--- src/main/java/model/TableMVT.java
+++ src/main/java/model/TableMVT.java
@@ -1,5 +1,41 @@
 package model;
 
 public class TableMVT {
+    private String proceso;
+    private int tamanio;
+    private int llegada;
+    private int duracion;
+    public TableMVT(String proceso, int tamanio, int llegada, int duracion) {
+        this.proceso = proceso;
+        this.tamanio = tamanio;
+        this.llegada = llegada;
+        this.duracion = duracion;
+    }
+    public String getProceso() {
+        return proceso;
+    }
+    public void setProceso(String proceso) {
+        this.proceso = proceso;
+    }
+    public int getTamanio() {
+        return tamanio;
+    }
+    public void setTamanio(int tamanio) {
+        this.tamanio = tamanio;
+    }
+    public int getLlegada() {
+        return llegada;
+    }
+    public void setLlegada(int llegada) {
+        this.llegada = llegada;
+    }
+    public int getDuracion() {
+        return duracion;
+    }
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+
+    
     
 }
